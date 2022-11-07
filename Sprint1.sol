c3ba05f67b37fea1859a6f501cf08e93cf66c87a
Remix Workspace <>
2022-11-07 15:23:27 +0300

    CALISIYOGIBIAMABILEMEDIM

--- Sprint1.sol
+++ Sprint1.sol
@@ -23,26 +23,19 @@ contract PowerContractInterface{
  function getTxOrigin() public view returns (address);
  function setCellsFromContract(int x, int y, bytes32 nonce) public payable returns (bytes32);
  function getTotalCalls() public view returns (uint);
-
 }
-
 contract BaBoBa{
 
     address powerContractAddres = 0xA7c40D644bDB571F98D72A08AF2A1Fb4Ab4f24fe;
     PowerContractInterface pc_interface = PowerContractInterface(powerContractAddres);
 
-   
    function arrangeContract(PowerContractInterface inter) public view returns(int){
         //address myAdd = inter.getMyTeamContract();
         //inter.setMyTeamContract(myAdd); 
         //bytes32 hs = inter.getCellHash(1,1);
-        int teamNumber = inter.getTeamNumberFromAccount(0x06D99001C37C88f996f629698c822F19dB68388D);
+        int teamNumber = inter.getMyTeamNumber();
         return teamNumber;
    }
-     
-   
    int a = arrangeContract(pc_interface);
-
-
-
+   int b = pc_interface.getMyTeamNumber();
 }
